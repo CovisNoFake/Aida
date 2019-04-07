ef53a82c96491c079e3597c0138375151aab92a9
CovisNoFake <hect.adio@gmail.com>
2019-04-07 10:16:42 -0500

    creacion Principal
    creacion de la pantalla princial

--- AIDA/AIDA/Login.xaml.cs
+++ AIDA/AIDA/Login.xaml.cs
@@ -13,22 +13,22 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0xc0a
+// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace AIDA
 {
     /// <summary>
-    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// Una página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class Login : Page
     {
-        public MainPage()
+        public Login()
         {
             this.InitializeComponent();
         }
-
-        private void IniciaSecion(object sender, RoutedEventArgs e ) {
-            Frame.Navigate(typeof(Login));
+        private void Inicia(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(Principal));
         }
     }
 }
